7b324ac4aa495b5169d83b6b48d905772ca6715a
Carolain Maccha <carolinegiselle26@hotmail.com>
2023-01-11 09:27:46 +0100

    bicicleta u3cm

---
+++ 2evaluacion/src/ejemploU3CM/PruebaBicicleta.java
@@ -0,0 +1,12 @@
+package ejemploU3CM;
+
+public class PruebaBicicleta {
+
+	public static void main(String[] args) {
+		Bicicleta miBicicleta=new Bicicleta(50,8,6);
+		Bicicleta tuBicicleta=new Bicicleta(50,7,9);
+		miBicicleta.acelerar();
+		tuBicicleta.frenar();
+	}
+
+}
